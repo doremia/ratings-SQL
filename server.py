b6151b168263d9307a5fd6ba33f50152f523cf2d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-30 13:00:03 -0700

    Last part of lab

--- server.py
+++ server.py
@@ -84,7 +84,65 @@ def logout():
     if session.get('user'):
         session['user'] = None
         flash('Logged out')
+    else:
+        flash('You are not logged in')
     return redirect('/')
+
+@app.route('/users/<user_id>')
+def show_use_info(user_id):
+
+    user = User.query.filter(User.user_id==user_id).one()
+
+    ratings_list = user.ratings #a list of ratings objects
+
+    # movie_list = []
+    # for rating in ratings_list:
+    #     movie_list.append(rating.movie) #list of movie objects
+
+
+    return render_template('show_info.html',
+                            user=user,
+                            ratings=ratings_list)
+
+@app.route('/movies')
+def show_movie_list():
+    
+    movies = Movie.query.all() #list of movie objects
+
+    return render_template('movie_list.html', movies = movies)
+
+@app.route('/movies/<movie_id>')
+def show_movie_info(movie_id):
+
+    movie = Movie.query.filter(Movie.movie_id == movie_id).one()
+
+    return render_template('show_movie_info.html', 
+                            movie = movie,
+                          )
+
+@app.route('/set-rating', methods=['GET'])
+def confirm_set_rating():
+    movie_id = request.args.get('movie_id')
+    if session.get('user'):
+        movie = Movie.query.filter(Movie.movie_id == movie_id).one()
+        return render_template('set_rating.html', movie=movie)
+    flash('You are not logged in!')
+    return redirect(f'/movies/{movie_id}')
+
+
+@app.route('/set-rating', methods=['POST'])
+def set_rating():
+    movie_id = request.form.get('movie_id')
+    score = int(request.form.get('score'))
+
+    email = session['user']
+    user = User.query.filter( User.email == email ).one()
+
+    #to be done: handle both the case where this is a new rating and 
+    #the case where this is updating an existing rating
+
+    return redirect(f'/movies/{movie_id}')
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -94,7 +152,7 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
-    app.config[DEBUG_TB_INTERCEPT_REDIRECTS] = False
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
